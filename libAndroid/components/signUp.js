4f7dbb3d8855143aa617cf6e4ff1731c23375fb0
Edgar Khanzadyan <edgar.khanzadyan@yandex.ru>
2017-01-18 13:22:05 +0400

    adding inputs in signIn and signUp scenes

--- libAndroid/components/signUp.js
+++ libAndroid/components/signUp.js
@@ -18,6 +18,32 @@ class Registration extends Component {
   render(){
     return(
       <View style={style.container}>
+        <View style={style.header}>
+          <TouchableOpacity 
+            style={style.headerTextContainer}
+            onPress={() => this.props.navigator.pop()}>
+            <Text style={style.headerText}>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.headerTextContainer}>
+            <Text style={style.headerText}>Done</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={style.smallContainer}>
+          <Text style={style.textStyle}>Full Name:</Text>
+          <TextInput 
+            style={style.textInputStyle}
+          />
+        </View>
+        <View style={style.smallContainer}>
+          <Text style={style.textStyle}>Email:</Text>
+          <TextInput 
+            style={style.textInputStyle}
+          />
+        </View>
+        <View style={style.smallContainer}>
+          <Text style={style.textStyle}>Password:</Text>
+          <TextInput style={style.textInputStyle}/>
+        </View>
       </View>
     );
   }
@@ -25,8 +51,31 @@ class Registration extends Component {
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0d6c9',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#f0d6c9'
+  },
+  header: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+  },
+  headerTextContainer: {
+    margin: 10
+  },
+  headerText: {
+    fontSize: 17
+  },
+  smallContainer: {
+    flexDirection: 'row',
+    marginBottom: 20
+  },
+  textInputStyle: {
+    width: 0.7 * width,
+    height: 40,
+  },
+  textStyle: {
+    width: 0.3 * width,
+    fontSize: 20
   }
 });
