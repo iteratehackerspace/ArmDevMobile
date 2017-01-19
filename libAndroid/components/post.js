5d2a2a2bedd50acf9d69e3c55fb01fb29d1ef435
Edgar Khanzadyan <edgar.khanzadyan@yandex.ru>
2017-01-19 15:12:35 +0400

    likes, comments in post, styling

--- libAndroid/components/post.js
+++ libAndroid/components/post.js
@@ -16,18 +16,51 @@ import { reduxStore } from '../containers/App';
 let store;
 export default
 class Post extends Component {
+  constructor(){
+    super();
+    this.state = {liked : false};
+  }
   _navigate(propName, name) {
   }
   componentWillMount() {
     store = reduxStore.getState().armDevMobile;
   }
   render(){
+    const getTags = store[0].post.tags.map((tag, idx) => {
+      return tag + '  ';
+    });
+    const getComments = store[0].post.comments.map((comment, idx) => {
+      return(
+        <View key={idx} style={style.commentContainer}>
+          <Image
+            style={style.imageStyle}
+            source={require('../assets/trump.jpg')}
+          />
+          <View>
+            <View style={style.authorTextContainer}>
+              <Text style={style.authorText}>{comment.author.fullName}</Text>
+            </View>
+            <View style={style.authorDateContainer}>
+              <Text>{comment.time}</Text>
+            </View>
+            <View style={style.commentTextContainer}>
+              <Text style={style.commentText}>{comment.text}</Text>
+            </View>
+          </View>
+        </View>
+      )
+    });
     return(
       <View style={style.container}>
         <ScrollView>
           <View style={style.postContainer}>
             <View style={style.titleContainer}>
               <Text style={style.title}>{store[0].post.title}</Text>
+            </View>
+            <View style={style.tagContainer}>
+              <Text style={style.tag}>
+                {getTags}
+              </Text>
             </View>
             <View style={style.authorContainer}>
               <Image
@@ -59,19 +92,28 @@ class Post extends Component {
               <View style={style.authorTextContainer}>
                 <Text style={style.authorText}>{store[0].post.author.fullName}</Text>
               </View>
-              <View style={style.authorDateContainer}>
-                <Text>{store[0].post.author.ShortDescription}</Text>
+              <View style={style.authorDescriptionContainer}>
+                <Text style={style.authorDescription}>{store[0].post.author.ShortDescription}</Text>
               </View>
             </View>
           </View>
+          <View style={style.aboutTheAuthorTextContainer}>
+            <Text style={style.aboutTheAuthorText}>Comments</Text>
+          </View>
+          {getComments}
         </ScrollView>
         <View style={style.footer}>
           <View style={style.upFoot}>
             <TouchableOpacity 
-              style={style.likesContainer}
-              activeOpacity={1}>
-              <Text style={style.likes}>Like|{store[0].post.likes}</Text>
+              style={this.state.liked ? style.likesContainerUnLike : style.likesContainerLike}
+              activeOpacity={1}
+              onPress={() => this.setState({liked: this.state.liked ? false : true}) }>
+              <Text style={this.state.liked ? style.likesUnLike : style.likesLike}>{this.state.liked ? `UnLike` : `Like`}|{store[0].post.likes}</Text>
             </TouchableOpacity>
+            <View 
+              style={style.commentsContainer}>
+              <Text style={style.comments}>Seen|{store[0].post.seen}</Text>
+            </View>
             <View 
               style={style.commentsContainer}>
               <Text style={style.comments}>Comments|{store[0].post.comments.length}</Text>
@@ -100,7 +142,10 @@ const style = StyleSheet.create({
   },
   titleContainer: {
     alignItems: 'center',
-    margin: 20
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
   },
   postContainer: {
     backgroundColor: 'white'
@@ -124,10 +169,11 @@ const style = StyleSheet.create({
   imageStyle: {
     width: 50,
     height: 50,
+    borderRadius: 1000,
+    marginLeft: 20,
   },
   authorContainer: {
     flexDirection: 'row',
-    marginLeft: 20,
   },
   authorTextContainer: {
     marginLeft: 10
@@ -138,14 +184,23 @@ const style = StyleSheet.create({
   authorDateContainer: {
     marginLeft: 10,
   },
-  likesContainer: {
+  likesContainerLike: {
     margin: 5,
     backgroundColor: '#DAA4E7',
     borderRadius: 5,
   },
-  likes: {
+  likesLike: {
     color: 'blue',
     margin: 2
+  },
+  likesContainerUnLike: {
+    backgroundColor: '#eed3d3',
+    borderRadius: 5,
+    margin: 5,
+  },
+  likesUnLike: {
+    color: '#bf0e0e',
+    margin: 2,
   },
   comments: {
     color: 'black',
@@ -156,6 +211,7 @@ const style = StyleSheet.create({
   },
   upFoot: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   aboutTheAuthorTextContainer: {
     marginTop: 5,
@@ -172,4 +228,39 @@ const style = StyleSheet.create({
     backgroundColor: 'white',
     flexDirection: 'row'
   },
+  footer: {
+    backgroundColor: 'white',
+    marginTop: 2
+  },
+  authorDescriptionContainer: {
+    marginLeft: 10,
+  },
+  authorDescription: {
+    color: 'black'
+  },
+  tagContainer: {
+    flexDirection: 'row',
+    marginLeft: 20,
+    marginBottom: 10,
+    marginRight: 20,
+    justifyContent: 'center',
+  },
+  tag: {
+    color: 'black',
+    fontWeight: '100',
+    fontSize: 12,
+  },
+  commentContainer: {
+    backgroundColor: 'white',
+    marginTop: 3,
+    flexDirection: 'row',
+  },
+  commentTextContainer: {
+    alignItems: 'center',
+    margin: 10,
+  },
+  commentText: {
+    fontSize: 14,
+    color: 'black',
+  }
 });
